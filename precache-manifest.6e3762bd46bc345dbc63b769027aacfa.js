c9c8ae01c46c104ace9986b6bec07dc1c5ff621e
YevhenSoldatov1998 <arr.ess1998@gmail.com>
2020-03-11 22:22:23 +0300

    Updates

---
+++ precache-manifest.6e3762bd46bc345dbc63b769027aacfa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cbb29e95d1109d1c268aec1bf542a4c",
+    "url": "/typesctiptTodolist/index.html"
+  },
+  {
+    "revision": "117172d3d638c62b1245",
+    "url": "/typesctiptTodolist/static/css/main.a6b7a73a.chunk.css"
+  },
+  {
+    "revision": "008136982ec46092ac49",
+    "url": "/typesctiptTodolist/static/js/2.bebf3599.chunk.js"
+  },
+  {
+    "revision": "117172d3d638c62b1245",
+    "url": "/typesctiptTodolist/static/js/main.e1aa0d0a.chunk.js"
+  },
+  {
+    "revision": "7955617460c90d0a7ab4",
+    "url": "/typesctiptTodolist/static/js/runtime~main.99b06877.js"
+  }
+]);
